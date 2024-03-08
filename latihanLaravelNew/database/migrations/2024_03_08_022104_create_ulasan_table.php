ce704d7b51a4db47003d7c8335bb734816955a99
riskiafiaa <riskiafia004@gmail.com>
2024-03-08 10:05:26 +0700

    Tugas 14: Laravel Migration

---
+++ latihanLaravelNew/database/migrations/2024_03_08_022104_create_ulasan_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ulasan', function (Blueprint $table) {
+            $table->id();
+            $table->text('ulasan');
+
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('users_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('film_id');
+            $table->foreign('film_id')->references('id')->on('film');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ulasan');
+    }
+};
